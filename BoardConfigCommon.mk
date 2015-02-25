646ded9a477f46de6f435b6aba1ab98b49be3f4f
Rashed Abdel-Tawab <rashed@linux.com>
2015-02-24 21:19:50 -0500

    z2: More arm64 BoardConfig fixes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,14 +24,17 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno430
 # CPU
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_VARIANT := cortex-a57
+# cortex-a57 doesn't have it's own set of bionic functions yet
+# so use generic until it does
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_2ND_ARCH := arm64
-TARGET_2ND_ARCH_VARIANT := armv8-a
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_2ND_CPU_ABI := arm64-v8a
+TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
